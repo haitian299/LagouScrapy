20054e4fd05ff3f1c2e3aa06336d1e4d615a0743
金海天 <haitian299@gmail.com>
2015-08-06 16:57:08 +0800

    adjust item according to database, next: PIPLINES

--- lagou/spiders/lagou_spider.py
+++ lagou/spiders/lagou_spider.py
@@ -4,6 +4,8 @@ from scrapy.spiders import Spider
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.selector import Selector
+import re
+import datetime
 
 from lagou.items import LagouItem
 
@@ -29,11 +31,21 @@ class LagouSpider(CrawlSpider):
         
         lagou['department'] = sel.xpath('//dl[@class="job_detail"]/dt/h1/div/text()').extract()[0]
 
-        lagou['salary'] = sel.xpath('//dd[@class="job_request"]/span[1]/text()').extract()[0]
+        salary = sel.xpath('//dd[@class="job_request"]/span[1]/text()').extract()[0]
+        salaryMinMax = re.findall('\d+',salary)
+        salaryMinMax = [int(i) for i in salaryMinMax]
+        lagou['salaryMin'] = min(salaryMinMax)
+        lagou['salaryMax'] = max(salaryMinMax)
 
         lagou['city'] = sel.xpath('//dd[@class="job_request"]/span[2]/text()').extract()[0]
 
-        lagou['workYear'] = sel.xpath('//dd[@class="job_request"]/span[3]/text()').extract()[0]
+        lagou['workYearMin'] = lagou['workYearMax'] = 0
+        workYear = sel.xpath('//dd[@class="job_request"]/span[3]/text()').extract()[0]
+        workYearMinMax = re.findall('\d+',workYear)
+        if len(workYearMinMax):
+            workYearMinMax = [int(i) for i in workYearMinMax]
+        lagou['workYearMin'] = min(workYearMinMax)
+        lagou['workYearMax'] = max(workYearMinMax)
 
         lagou['education'] = sel.xpath('//dd[@class="job_request"]/span[4]/text()').extract()[0]
 
@@ -41,7 +53,16 @@ class LagouSpider(CrawlSpider):
 
         lagou['advantage'] = sel.xpath('//dd[@class="job_request"]/text()').extract()[5].rstrip().lstrip()
 
-        lagou['postTime'] = sel.xpath('//dd[@class="job_request"]/div/text()').extract()[0]
+        posttime = sel.xpath('//dd[@class="job_request"]/div/text()').extract()[0]
+        date = re.findall('\d+',posttime)
+        if len(date) == 1:
+            days = int(date[0])
+            lagou['postTime'] = datetime.date.today() - datetime.timedelta(days)
+        elif len(date) == 3:
+            lagou['postTime'] = datetime.date(int(date[0]),int(date[1]),int(date[2]))
+        else:
+            lagou['postTime'] = datetime.date.today()
+
 
         descriptionRaw = sel.xpath('//dd[@class="job_bt"]//p').extract()
         description = ''
@@ -57,7 +78,11 @@ class LagouSpider(CrawlSpider):
 
         lagou['companyField'] = sel.xpath('//dl[@class="job_company"]/dd/ul[1]/li[1]/text()').extract()[0]
 
-        lagou['companyPopulation'] = sel.xpath('//dl[@class="job_company"]/dd/ul[1]/li[2]/text()').extract()[0]
+        companyPopulation = sel.xpath('//dl[@class="job_company"]/dd/ul[1]/li[2]/text()').extract()[0]
+        PopMinMax = re.findall('\d+',companyPopulation)
+        PopMinMax = [int(i) for i in PopMinMax]
+        lagou['companyPopulationMin'] = min(PopMinMax)
+        lagou['companyPopulationMax'] = max(PopMinMax)
 
         lagou['companyUrl'] = sel.xpath('//dl[@class="job_company"]/dd/ul[1]/li[3]/a/@href').extract()[0]
 
